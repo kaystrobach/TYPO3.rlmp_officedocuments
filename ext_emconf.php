428cb48682ec54701fa597c84623fba5151fd65b
Kay Strobach <info@kay-strobach.de>
2014-05-22 09:03:20 +0200

    [TASK] new ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,39 +1,48 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "rlmp_officedocuments".
-#
-# Auto generated 04-10-2011 02:00
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "rlmp_officedocuments".
+ *
+ * Auto generated 15-05-2014 16:42
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Documents Suite',
 	'description' => 'Provides a frontend plugin for displaying office documents and (in a future version) a backend module for importing them into the page tree.',
 	'category' => 'fe',
-	'shy' => 1,
-	'version' => '0.1.5',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'TYPO3_version' => '3.0.0-',
-	'PHP_version' => '3.0.0-',
-	'module' => 'mod1,cm1',
-	'state' => 'beta',
-	'uploadfolder' => 1,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 1,
-	'lockType' => '',
 	'author' => 'Robert Lemke, Kay Strobach',
 	'author_email' => 'typo3@kay-strobach.de',
 	'author_company' => 'Kay Strobach',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'version' => '0.0.1',
+	'constraints' =>
+	array (
+		'depends' =>
+		array (
+			'typo3' => '4.5-6.1',
+		),
+		'conflicts' =>
+		array (
+		),
+		'suggests' =>
+		array (
+		),
+	),
 );
 
 ?>
